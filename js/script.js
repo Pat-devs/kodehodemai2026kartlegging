71375a3fbad04edfb4287f7d57bf8ddfe1a46f4a
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-21 10:21:54 +0200

    try some comments

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// javascript kommentar 
+console.log("hei");
+
+// hvis jeg vil 
+// ha en lang 
+// tekst
+
+/* Generate SLSA3 
+provenance for your 
+existing release
+ workflows 
+*/
+
+
+// console.log("test");
+
+/* 
+function tull() {}
+
+CompressionStream.logw */
