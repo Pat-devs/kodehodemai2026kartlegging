490489d7bf6e7436ef4bc74e56375159979f716f
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-21 11:17:07 +0200

    examples of getting button and changing classnames

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-// javascript kommentar 
-console.log("hei");
+// problemstilling: hent knappen, sånt at vi kan gjøre noe med den
 
-document.querySelector("h1").textContent = "Ingenting"
+// get the button and store it as a variable for later use
+let buttonElement = document.querySelector("button");
+
+
+buttonElement.textContent = "DO NOT CLICK IT!"
+
+// buttonElement.style = "color: white; background-color: red;"
+
+// change class of an element:
+// in html we would: add proprty: class="button-red"
+
+// in JS, we can use className:
+buttonElement.className = "button-red";
