6e0ec4667d3c54b9479c8933e77db28392b96fea
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-21 12:34:03 +0200

    custom function examples

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // problemstilling: hent knappen, sånt at vi kan gjøre noe med den
 
+
+
 // get the red and store it as a variable for later use
 let buttonElementRed = document.querySelector(".button-red");
 
 // get the button and store it as a variable for later use
 let buttonElementGreen = document.querySelector(".button-green");
 
+// funksjoner i javascript
+// utføre eller gjenta handlinger
 
+// lag en egen funsksjon:
 
+// basic greeting funksjon, skriver ut en melding
+function greet(message) { 
+    console.log(message)
+}
 
-console.log(buttonElementGreen)
+// greet("Hei")
+
+// avansert greeting funskjon
+// kan ta imot, et navn, og en hilsen tekst
+
+function greetAdvanced(userName, greetingText) {
+
+    console.log(greetingText, userName)
+}
+
+greetAdvanced("Patryk", "God dag") // ønsket resultat: "God dag Patryk"
